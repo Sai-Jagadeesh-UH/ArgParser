e068e79955d3fd3931d0fb523c7f260f2454e2b1
Sai Jagadeesh Yadavalli <112056586+Sai-Jagadeesh-UH@users.noreply.github.com>
2024-12-24 21:05:14 +0000

    entry script updated

---
+++ passman.py
@@ -0,0 +1,33 @@
+from src.passman import parser, OptionObject
+from src.passman import list_items,show_item,delete_item,copy_item,update_item,add_item
+
+
+def main():
+
+    args = parser.parse_args()
+
+    if (ErrorString := OptionObject(args).getExceptionString()):
+        print(ErrorString)
+        exit()
+
+    if args.Item != "":
+        if args.add:
+            add_item(args.Item, args)
+        elif args.update:
+            update_item(args.Item)
+
+        if args.copy:
+            copy_item(args.Item)
+        if args.delete:
+            delete_item(args.Item)
+
+        if not any([args.add, args.update, args.copy, args.delete, args.list]):
+            show_item(args.Item)
+    else:
+        if args.list:
+            list_items()
+
+
+# clean()
+if __name__ == "__main__":
+    main()
